446ba84cec67e0b988c15b88c83c68c731b866d2
diegopajarito <diegopajarito@gmail.com>
2019-03-07 08:51:49 +0100

    this is the firs attempt to make it from the QGIS python console

--- init.py
+++ init.py
@@ -4,18 +4,33 @@ import subprocess
 
 print('lets try, yes its me')
 os.system("ls -l")
-print('lets try, now the path')
-os.system("pwd")
-
-print('lets try, now as variable')
-txt_out = os.system("pwd")
-
-#print('The value of txt_out is: ' + txt_out)
-
 
 test = subprocess.Popen(["ping","-W","2","-c", "1", "192.168.1.70"], stdout=subprocess.PIPE)
 output = test.communicate()[0]
 
-print(test)
 
 #https://stackoverflow.com/questions/3730964/python-script-execute-commands-in-terminal/3731000
+
+# And now with geotriples
+# Variables
+geotriples = "geotriples-cmd"
+action = "generate_mapping"
+b_command = "-b"
+base_uri = "'http://diana.test.co/repo/'"
+o_command = "-o"
+path_rml_file = "'/home/diego/Documents/Maps/geotriples/test_rml_py.rml'"
+path_shp_file = "'/home/diego/Documents/Maps/geotriples/testgeotriples.shp'"
+
+# geotriples-cmd generate_mapping -b http://test.caro.fi/repo/ -o '/home/diego/Documents/Maps/geotriples/test_rml.rml'  '/home/diego/Documents/Maps/geotriples/testgeotriples.shp'
+
+
+test = subprocess.Popen([geotriples, action, b_command, base_uri, o_command, path_rml_file, path_shp_file], stdout=subprocess.PIPE)
+output = test.communicate()[0]
+
+print(output)
+
+sentence = [geotriples, action, b_command, base_uri, o_command, path_rml_file, path_shp_file]
+joined = ' '.join(sentence)
+print(joined)
+
+os.system(joined)
---
+++ qgis_terminal_script.py
@@ -0,0 +1,42 @@
+# Licence: CC-BY
+# Description: This script serves to test the execution of console-based 
+# commands from QGIS. In particular the use of geotriples jar
+# Tested only for Ubuntu and QGIS 3
+#
+# Comments: It requires geotriples jar to be installed. Geotriples instructions
+# used are here: https://github.com/LinkedEOData/GeoTriples/wiki
+# Authors: Carolina Figueroa - Diego Pajarito 
+
+import subprocess
+from qgis.core import *
+import qgis.utils
+
+# Setting up the input values
+lyr_name = 'sint'
+
+# Setting up the veariables needed by geotriples
+geotriples = 'geotriples-cmd'
+action = "generate_mapping"
+b_command = "-b"
+base_uri = "%r" % 'http://diana.test.co/repo/'
+o_command = "-o"
+path_rml_file = "%r" % '/home/diego/Documents/Maps/geotriples/test_rml_py.rml'
+
+
+lyr = QgsProject.instance().mapLayersByName(lyr_name)
+if lyr:
+    lyr_path = lyr[0].dataProvider().dataSourceUri()
+    path, sep, tail = lyr_path.partition('|')
+    lyr_path = "%r" % path
+
+    # Now call the command
+    sentence = [geotriples, action, b_command, base_uri, o_command, path_rml_file, lyr_path]
+    joined = ' '.join(sentence)
+    print(joined)
+    sbp = subprocess.Popen([geotriples, action, b_command, base_uri, o_command, path_rml_file, lyr_path], stdout=subprocess.PIPE)
+    output = sbp.communicate()[0]
+    print(output)
+    
+else:
+    print('Error: No layer found')
+    
